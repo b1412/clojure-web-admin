4b78fd81a3a5a19adefdf727271dc6396585ecc3
LeonZhou <zhouleib1412@gmail.com>
2015-12-06 12:48:09 +1300

    add demo.sql

--- db/init.sql
+++ db/init.sql
@@ -15,6 +15,13 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+DROP DATABASE IF EXISTS `clojure_web`;
+
+CREATE DATABASE `clojure_web` charset utf8;
+
+USE `clojure_web`;
+
+
 --
 -- Table structure for table `dictionary`
 --
